c173fc74a852030b9bc0d419bfb92e177a9393bd
Zahragrs <dryagaroosi@gmail.com>
2025-07-30 11:05:33 +0330

    add comment for me

--- books/models.py
+++ books/models.py
@@ -14,3 +14,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment on {self.book.title}"
+
+
+# خط12 :
+# یعنی هر کامنت به یک کتاب مرتبط است. این رابطه‌ی ManyToOne (هر کتاب چند کامنت) است
+#related_name='comments' باعث می‌شود که از طرف کتاب هم بتوانیم به همه کامنت‌ها دسترسی داشته باشی
+# مثل book.comments.all()
+# در این مدل، رابطه book با Book از نوع ForeignKey است  یعنی ManyToOne 
+#
--- books/views.py
+++ books/views.py
@@ -6,3 +6,13 @@ from .serializers import CommentSerializer
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.select_related('book').all()
     serializer_class = CommentSerializer
+
+
+# خط7:
+# چرا از select_related استفاده شده؟
+#چون در مدل Comment، فیلد book از نوع ForeignKey هست
+# یعنی رابطه ManyToOne
+#وقتی از select_related('book') استفاده می‌کنیم، در یک کوئری SQL، اطلاعات مربوط به کتاب هم لود میشه
+# select_related:
+#همه‌ی کامنت‌ها و کتاب‌های مربوطه رو با فقط 1 کوئری ترکیبی میاره
+#فقط 1 کوئری
